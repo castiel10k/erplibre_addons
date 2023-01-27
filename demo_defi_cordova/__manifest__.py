c041baa885e28ba34d38b8f290cb304c6a2489ca
castiel <castielcf@gmail.com>
2023-01-27 11:56:44 -0500

    [WIP] Cordova testing

---
+++ demo_defi_cordova/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Demo Cordova",
+    "category": "Uncategorized",
+    "version": "12.0.1.0",
+    "author": "TechnoLibre",
+    "license": "AGPL-3",
+    "website": "https://technolibre.ca",
+    "application": True,
+    "depends": ["mail"],
+    "data": [
+        "views/demo_model_2_internal.xml",
+    ],
+    "installable": True,
+}
